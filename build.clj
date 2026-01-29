2413490d29fdd857471ce78fe5fd9963cc40fd75
Zubair Ahmed <zahmed0808@gmail.com>
2026-01-29 12:56:16 -0800

    build stuff

---
+++ build.clj
@@ -0,0 +1,36 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'io.github.your-org/clj-font-awesome)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (clean nil)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]
+                :resource-dirs ["resources"]
+                :scm {:url "https://github.com/your-org/clj-font-awesome"
+                      :connection "scm:git:git://github.com/your-org/clj-font-awesome.git"
+                      :developerConnection "scm:git:ssh://git@github.com/your-org/clj-font-awesome.git"
+                      :tag (str "v" version)}})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file})
+  (println "Built JAR:" jar-file))
+
+(defn deploy [_]
+  (jar nil)
+  (dd/deploy {:installer :remote
+              :artifact jar-file
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
